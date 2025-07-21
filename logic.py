c9a71f0be9b6b57f4494973c8da834053dfb1541
Midnight-Rg <teonundunyasi@gmail.com>
2025-07-21 20:40:33 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,12 +1,12 @@
 import aiohttp
 import random
 from random import randint
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Pokemon:
     pokemons = {}
 
-    def _init_(self, pokemon_trainer):
+    def __init__(self, pokemon_trainer):
         self.pokemon_trainer = pokemon_trainer
         self.pokemon_number = random.randint(1, 1000)
         self.name = None
@@ -14,8 +14,19 @@ class Pokemon:
         self.power = random.randint(30, 60)
         self.hp = random.randint(200, 400)
         self.last_feed_time = datetime.now()
-        if pokemon_trainer not in self.pokemons:
-            self.pokemons[pokemon_trainer] = self
+
+        if pokemon_trainer not in Pokemon.pokemons:
+            Pokemon.pokemons[pokemon_trainer] = self
+
+    async def feed(self, feed_interval=20, hp_increase=10):
+        current_time = datetime.now()
+        delta_time = timedelta(hours=feed_interval)
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Pokémon sağlığı geri yüklendi. Mevcut HP: {self.hp}"
+        else:
+            return f"Pokémonunuzu şu zaman besleyebilirsiniz: {self.last_feed_time + delta_time}"
 
     async def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -31,8 +42,8 @@ class Pokemon:
         if not self.name:
             self.name = await self.get_name()
         return f"""Pokémon ismi: {self.name}
-                Pokémon gücü: {self.power}
-                Pokémon sağlığı: {self.hp}"""
+Pokémon gücü: {self.power}
+Pokémon sağlığı: {self.hp}"""
 
     async def show_img(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -49,22 +60,21 @@ class Pokemon:
         if isinstance(enemy, Wizard):
             chance = randint(1, 5)
             if chance == 1:
-                return "Sihirbaz Pokémon, savaşta bir kalkan kullandı!"
+                return "Sihirbaz Pokémon savaşta bir kalkan kullandı!"
         if enemy.hp > self.power:
             enemy.hp -= self.power
-            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ne saldırdı\n@{enemy.pokemon_trainer}'nin sağlık durumu şimdi {enemy.hp}"
+            return f"Pokémon eğitmeni @{self.pokemon_trainer}, @{enemy.pokemon_trainer}'ye saldırdı.\n@{enemy.pokemon_trainer}'nin sağlık durumu şimdi {enemy.hp}"
         else:
             enemy.hp = 0
-            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ni yendi!"
+            return f"Pokémon eğitmeni @{self.pokemon_trainer}, @{enemy.pokemon_trainer}'yi yendi!"
+
 
 class Wizard(Pokemon):
-    def _init_(self, pokemon_trainer):
-        super()._init_(pokemon_trainer)
+    # Sihirbaz Pokémon’a özel şeyler buraya eklenebilir
+    pass
+
 
 class Fighter(Pokemon):
-    def _init_(self, pokemon_trainer):
-        super()._init_(pokemon_trainer)
-
     async def attack(self, enemy):
         super_power = randint(5, 15)
         self.power += super_power
